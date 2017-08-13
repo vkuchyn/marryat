68647c680aacbd6b0ea4033c8db53b4ab64117ac
Viktor Kuchyn <kuchin.victor@gmail.com>
2017-08-14 01:00:04 +0300

    Created reservation controller system test

---
+++ src/main/java/com/keypr/marryat/web/ReservationController.java
@@ -0,0 +1,19 @@
+package com.keypr.marryat.web;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author viktor email kuchin.victor@gmail.com
+ */
+@RestController
+@RequestMapping(consumes = "application/json")
+public class ReservationController {
+
+    @PostMapping("/reservations")
+    public void reserveRoom(@RequestBody final String json) {
+        //TODO(vkuchyn) implement feature
+    }
+}
