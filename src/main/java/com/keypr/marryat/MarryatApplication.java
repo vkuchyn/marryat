51fb9e54c4073645c1be0b49d7014f7f783a73b6
Viktor Kuchyn <kuchin.victor@gmail.com>
2017-08-13 22:41:43 +0300

    Created spring boot project

---
+++ src/main/java/com/keypr/marryat/MarryatApplication.java
@@ -0,0 +1,12 @@
+package com.keypr.marryat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarryatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarryatApplication.class, args);
+    }
+}
